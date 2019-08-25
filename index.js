db33be112d2467b34c31e000077095c564f02867
jason <jasonedlewis@gmail.com>
2019-08-24 23:58:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+let thanks = [];
+let writeCards = (arr, event)=>{
+    for (let i = 0; i < arr.length; i++) {
+        thanks.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return thanks;
+};
+
+let countdown = (num) =>{
+   
+    
+    while (0 <= num) {
+        
+        console.log(num);
+        num -=1
+    }
+
+
+}
