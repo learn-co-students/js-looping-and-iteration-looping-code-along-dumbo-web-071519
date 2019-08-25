a281ff992c2e2849f2071b1659c5bb890639618a
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-08-25 15:38:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let arr = []
+    for (let i = 0; i<names.length; i++){
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arr
+}
+function countdown(num) {
+    let i = num;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+
+    
+}
